ffd77084f371418cd88fcba8f9d47b21c51ca611
Surya-V-28 <suryaarmy231@gmail.com>
2022-04-23 07:34:50 +0530

    init commit

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header >
+     <h1> welcome to first project </h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
